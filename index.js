cfa7a786217e7e806f059809fb8698ab90ba93dc
Alies Lapatsin <lptn@users.noreply.github.com>
2021-02-19 17:09:17 +0300

    Bypass "Address already in use" issue
    Error: Command failed with exit code 1: ssh-agent -a /tmp/ssh-auth.sock
    bind: Address already in use
    unix_listener: cannot bind to path: /tmp/ssh-auth.sock

--- index.js
+++ index.js
@@ -17,11 +17,10 @@ function ssh() {
 
   if (!fs.existsSync(ssh)) {
     fs.mkdirSync(ssh)
+    let authSock = '/tmp/ssh-auth.sock'
+    execa.sync('ssh-agent', ['-a', authSock])
+    core.exportVariable('SSH_AUTH_SOCK', authSock)
   }
-
-  let authSock = '/tmp/ssh-auth.sock'
-  execa.sync('ssh-agent', ['-a', authSock])
-  core.exportVariable('SSH_AUTH_SOCK', authSock)
 
   let privateKey = core.getInput('private-key').replace('/\r/g', '').trim() + '\n'
   execa.sync('ssh-add', ['-'], {input: privateKey})
